3eb7981e7418e641eb7231da677edd7c872b5664
hajersayeb <148336396+sirine-mansour@users.noreply.github.com>
2023-11-14 12:07:02 +0100

    variables and functions js
    test des donnees

---
+++ js/min.js
@@ -0,0 +1,41 @@
+//recuperation des champs du formulaire
+const fullName = document.querySelector('.fullName'); //recuperation de champs fullName
+const dateOfBirth = document.querySelector('.dateOfBirth');
+const gender= document.querySelector('select[name="gender"]');
+const task1 = document.querySelector('.task1');
+const task2 = document.querySelector('.task2');
+const task3 = document.querySelector('.task3');
+ //recuperation des boutons
+const result = document.querySelector('.result');
+const submit = document.querySelector('.submit');
+const reset = document.querySelector('.reset');
+//validation rules fullName
+function validatonfullName (fullName) {
+// check if the full name is required
+    if(!fullName) {
+        return "Full name is required.";
+    }
+    // check the legth of the full name
+    if (fullName.length < 3 || fullName > 20 ){
+   return "Full name must be between 3 and 20 characters long.";}
+   //check the format of the full name
+   if (!/^[a-zA-Z]+$/.test(fullName)) {
+      return "Full name must contain only letters and spaces.";
+   }
+   return null;
+}
+function validationDate(date) {
+    if (!date) {
+        return "Date ids required .";
+    }
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) {
+ return "Date must be in the format YYYY-MM-DD";
+    }
+    const today = new Date ();
+    const minDate = new Date (today.getFullYear(),today.getMonth() ,today.getDate());
+    const maxDate = new Date (today.getFullYear() +1,today.getMonth() ,today.getDate());
+    if (date < minDate || date > maxDate) {
+        return "Date must be between today and one year from today";
+}
+    return null;
+}
