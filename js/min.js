7067636de7217f4f65f06199b596a7ecfa220c68
hajersayeb <148336396+sirine-mansour@users.noreply.github.com>
2023-11-19 15:35:48 +0100

    result
    functions

--- js/min.js
+++ js/min.js
@@ -29,16 +29,15 @@ function validateDate(date) {
     if (!date) {
         return "Date is required.";
     }
-    if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) {
- return "Date must be in the format YYYY-MM-DD.";
+    if(!/^\d{4}-\d{2}-\d{2}$/.test(date)) {
+        return "date must be in the format YYYY-MM-DD"
     }
-    const today = new Date ();
-    const minDate = new Date (today.getFullYear(), today.getMonth(), today.getDate());
-    const maxDate = new Date (today.getFullYear() +1, today.getMonth(), today.getDate());
-    if (date < minDate || date > maxDate) {
-        return "Date must be between today and one year from today.";
-}
+    const today =  new Date();
+    if(date > today){
+        return"Date must be between today and one year from today";
+    }
     return null;
+
 }
 function validateGender(gender) {
     if(!gender) {
@@ -52,8 +51,19 @@ function validateGender(gender) {
     return null;
 }
 
+
+
 // afficher les valeurs des champs
 submit.addEventListener('click' , (event) => {
+    //calculer le resultat
+ const total = ((parseInt(task1.value) + parseInt(task2.value) + parseInt(task3.value)) /3);
+    console.log(dateOfBirth.value)
+    const today =  new Date();
+    const minDate=new Date (today.getFullYear(), today.getMonth(), today.getDate());
+    const maxDate=new Date (today.getFullYear() +1, today.getMonth(), today.getDate());
+    console.log(maxDate);
+    console.log (minDate);
+    console.log(today)
     if ( validatefullName(fullName.value) !== null) {
         alert (validatefullName (fullName.value))
         return;
@@ -62,6 +72,31 @@ submit.addEventListener('click' , (event) => {
         alert (validateDate (dateOfBirth.value))
     return;
 }
+if (validateGender (gender.value) !== null) {
+    alert (validateGender (gender.value))
+return;
+}
 
-})
+// Afficher le resultat dans le champ "Result"
+result.innerHTML =
+` <h1>Full Name :</h1>${fullName.value}
+ <h1>Birth date :</h1>${dateOfBirth.value}
+ <h1>Gender :</h1>${gender.value}
+ <h1>Average:</h1>${total}
+ <h1>Mark:</h1>${total >=10 ?"Succeed" :"Fail"} 
+ 
+ `;
+ 
+ 
+ 
 
+});
+reset.addEventListener('click', () => {
+    result.innerHTML = ""
+    fullName.value =""
+    dateOfBirth.value =""
+    task1.value =""
+    task2.value =""
+    task3.value =""
+ });
+ 
